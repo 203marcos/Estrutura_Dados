8881f16fd8567338cd2b5bc83da7fa696e951238
Marcos Dias <marcosaurelioce@hotmail.com>
2024-02-28 17:31:33 -0300

    teste1

--- Main.java
+++ Main.java
@@ -2,24 +2,12 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void permuta(String p, String s) {
-        int n = s.length();
-
-        if (n == 0)
-            System.out.print(p + " ");
-        else
-            for (int i = 0; i < n; i++)
-                permuta(p + s.charAt(i), s.substring(0, i) + s.substring(i+1));
-    }
-
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        String conjunto;
-
-        System.out.print("Informe um conjunto de símbolos a ser permutado: ");
-        conjunto = entrada.next();
-
-        System.out.print("Permutações: ");
-        permuta("", conjunto);
+        ListaMarcos arroz = new ListaMarcos(10);
+        arroz.add(1);
+        arroz.add(2);
+        arroz.add(4);
+        arroz.add(2,3);
+        arroz.retornaTodos();
     }
 }
